327c6fe4ad1dae650e21ece9a05d468cc60edb9a
jokhab <almalki.yah@gmail.com>
2015-12-29 15:59:52 +0300

    Create addon.setup.php

---
+++ addon.setup.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+	'author' => 'Akwadnet',
+	'author_url' => 'https://akwad.net',
+	'description' => 'fix the url title to sumilate arabic words!',
+	'docs_url' => 'http://akwad.net/rtl/documentation',
+	'name' => 'URL Title Arabic Fix',
+	'namespace' => 'Akwadnet\RTLFIX',
+	'version' => '1.0.4'
+);
